17a8d41898be94c06e77e032330328c50657d299
hy <lshhwj@gmail.com>
2019-04-09 15:27:09 +0800

    fix(root): robust folder traversal.

--- install.sh
+++ install.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-[[ $1 = "-D" ]] && set -x
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $HERE/common.sh
+[[ $1 = "-D" ]] && set -x
 
-cd $HERE
-for d in $(ls -1d */); do # NOTE: $d contains the tailing /, ${d%%/} does not
-    if [[ -r ${d%%/}/install.sh ]]; then
-        echo "[INFO] Found ${d%%/}/install.sh, installing:" 
-        try bash ${d%%/}/install.sh "$@"
+find $HERE -mindepth 1 -maxdepth 1 -type d -print0 | while read -d '' -r d; do
+    if [[ -r ${d}/install.sh ]]; then
+        echo "[INFO] Found ${d}/install.sh, installing:"
+        try bash ${d}/install.sh "$@"
     fi
 done
