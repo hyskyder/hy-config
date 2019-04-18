f2cd7c1ac07485abca232ceabfb9f8433b74e3ee
hy <lshhwj@gmail.com>
2019-04-18 15:59:09 +0800

    fix(scripts): add wrapper to find abs path.

--- hy_scripts/scripts/teleself.sh
+++ hy_scripts/scripts/teleself.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
-HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-[[ $1 = "-D" ]] && shift && set -x
-
-function if_is_knwon_command {
-    KNOWN_CMD=("msg" "fwd" "fwd_media" "send_audio" "send_document" "send_file" "send_photo" "send_text" "send_video")
-    for c in "${KNOWN_CMD[@]}" ; do 
-        [[ $1 == $c ]] && return 0
-    done 
-    return 1
+function get_abosolute_path {
+    local SOURCE="${1}"
+    local DIR=""
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    echo $DIR
 }
 
-TELE_CMD="msg"
-if_is_knwon_command $1 && \
-    TELE_CMD=$1 && \
-    shift
-
-[[ -f ${HERE}/teleself.config.ini ]] && SELF_ID=$(cat teleself.config.ini)
-[[ -z ${SELF_ID} ]] && read -p "Your Telegram User id#:" SELF_ID
-
-telegram-cli -W -e "${TELE_CMD} user#${SELF_ID} $*"
+[[ $1 = "-D" ]] && shift && set -x
+HERE=$(get_abosolute_path ${BASH_SOURCE[0]})
+. ${HERE}/../../common.sh
+(
+    cd $HERE
+    try bash teleself.sh.real "${@}"
+)
