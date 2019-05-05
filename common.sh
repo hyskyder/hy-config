ab85738e36cfd149fe39174d7736705a19233b9a
hy <lshhwj@gmail.com>
2019-05-05 22:24:09 +0800

    feat: improve root install script

--- common.sh
+++ common.sh
@@ -14,3 +14,14 @@ function error {
     echo "[Error] Line $func_caller:" "$@" >&2
     exit 1
 }
+
+function get_abosolute_path {
+    local SOURCE="${1}"
+    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+        local DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+        SOURCE="$(readlink "$SOURCE")"
+        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+    done
+    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+    return $DIR
+}
--- install.sh
+++ install.sh
@@ -2,11 +2,46 @@
 
 HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 . $HERE/common.sh
-[[ $1 = "-D" ]] && set -x
 
-find $HERE -mindepth 1 -maxdepth 1 -type d -print0 | while read -d '' -r d; do
-    if [[ -r ${d}/install.sh ]]; then
-        echo "[INFO] Found ${d}/install.sh, installing:"
-        try bash ${d}/install.sh "$@"
-    fi
-done
+function usage {
+    cat << EndOfUsageMsg 
+$0 : Intall my_init
+
+Usage: $0 [Option] [folders_list] ...
+  Option: 
+    -a  Install all folders here.
+    -D  Debug flag.
+    -h  This help msg.
+EndOfUsageMsg
+}
+
+while getopts ":aDh" opt; do
+case ${opt} in
+    a ) TARGET_ALL=true ;;
+    D ) DEBUG_FLAG="-D" ; set -x ;;
+    h ) usage ;;
+    : ) echo "Invalid option: $OPTARG requires an argument" 1>&2 ; exit 1 ;;
+    \? ) echo "Invalid Option: -$OPTARG" 1>&2 ; exit 1 ;;
+esac done
+shift $((OPTIND -1))
+
+TARGET_ALL=${TARGET_ALL:-false}
+DEBUG_FLAG=${DEBUG_FLAG:-""}
+
+if ${TARGET_ALL}; then
+    find $HERE -mindepth 1 -maxdepth 1 -type d -print0 | while read -d '' -r d; do
+        if [[ -r ${d}/install.sh ]]; then
+            echo "[INFO] Found ${d}/install.sh, installing:"
+            try bash ${d}/install.sh ${DEBUG_FLAG}
+        fi
+    done
+elif [[ $# -gt 0 ]]; then
+    for d in "$@" ; do
+        if [[ -r ${d}/install.sh ]]; then
+            echo "[INFO] Found ${d}/install.sh, installing:"
+            try bash ${d}/install.sh ${DEBUG_FLAG}
+        fi
+    done
+else
+    usage
+fi
