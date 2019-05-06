ab27f3b65844f4048d3d395788e59a015e07aa3c
hy <lshhwj@gmail.com>
2019-05-06 22:04:49 +0800

    feat: remove indirect wrapper script

--- bin_scripts/scripts/teleself.sh
+++ bin_scripts/scripts/teleself.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-function get_abosolute_path {
-    local SOURCE="${1}"
-    local DIR=""
-    while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-        DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-        SOURCE="$(readlink "$SOURCE")"
-        [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-    done
-    DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-    echo $DIR
+HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+# ABS_SCRIPT_PATH="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")" )"
+[[ $1 = "-D" ]] && shift && set -x
+
+function if_is_known_command {
+    KNOWN_CMD=("msg" "fwd" "fwd_media" "send_audio" "send_document" "send_file" "send_photo" "send_text" "send_video")
+    for c in "${KNOWN_CMD[@]}" ; do 
+        [[ $1 == $c ]] && return 0
+    done 
+    return 1
 }
 
-[[ $1 = "-D" ]] && shift && set -x
-HERE=$(get_abosolute_path ${BASH_SOURCE[0]})
-. ${HERE}/../../common.sh
-(
-    cd $HERE
-    try bash teleself.sh.real "${@}"
-)
+TELE_CMD="msg"
+if_is_known_command $1 && \
+    TELE_CMD=$1 && \
+    shift
+
+[[ -f ${HERE}/teleself.config.ini ]] && SELF_ID=$(cat ${HERE}/teleself.config.ini)
+[[ -z ${SELF_ID} ]] && read -p "Your Telegram User id#:" SELF_ID
+
+telegram-cli -W -e "${TELE_CMD} user#${SELF_ID} $*"
