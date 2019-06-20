8c264b035621cd2ebc1ce48abd28e8b68be904ba
hy <lshhwj@gmail.com>
2019-06-20 22:53:08 +0800

    refactor(script): bibtex processing.

--- bin_scripts/scripts/bib-abbr.py
+++ bin_scripts/scripts/bib-abbr.py
@@ -4,36 +4,43 @@ import os, sys
 from Clerical.latex import abbrev_author_list, simple_process_bib
 from functools import partial
 
-simple_process_bib=partial(simple_process_bib,
+process_bib=partial(simple_process_bib,
     exclude=["abstract","file","keywords"],
     commentify=["booktitle","publisher","month","url","urldate","address","series"]
 )
 
-def usage():
-    print("""Usage:
-{self} /path/to/a_paper.bib > /path/to/processed.bib
-{self} <author list>
-
-E.g:
-
-$ {self} Family Name, with Given-Names and Apple Juice, Bad and Orange Juice, Good and Lucas, Ahmad-Reza and My Corporation
->>> Family Name, W. G.-N. and Apple Juice, B. and Orange Juice, G. and Lucas, A.-R. and My Corporation
-""".format(self=sys.argv[0]))
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description='Bibtxt Helper')
+    parser.add_argument('f', metavar='...', type=str, nargs='*',
+                    help='<File> or <Author list ...>')
+    parser.add_argument("-a","--keep-author", action='store_true',
+                    default=False,
+                    help='Set to not to shorten author list.')
+    parser.add_argument("-i","--stdin", action='store_true',
+                    default=False,
+                    help='Set to read from Stdin.')
+        
+    args = parser.parse_args()
+    if not args.stdin and not args.f:
+        parser.print_usage()
+    return args
 
 if __name__=="__main__":
     """
     """
-    if len(sys.argv)==2 and sys.argv[1]=="-i":
+    args=parse_args()
+
+    if args.stdin:
         print("[NOTE] read input from stdin",file=sys.stderr)
-        stream=sys.stdin
-        for outline in simple_process_bib(stream):
-            print(outline,end="")
-    elif len(sys.argv)==2 and os.path.isfile(sys.argv[1]):
-        with open(sys.argv[1],"r") as instream:
-            for outline in simple_process_bib(instream):
-                print(outline,end="")
-    elif len(sys.argv)>=2:
-        input_str=" ".join(sys.argv[1:])
-        print(abbrev_author_list(input_str))
+        instream=sys.stdin
+    elif len(args.f)==1 and os.path.isfile(args.f[0]):
+        instream=open(args.f[0],"r")
     else:
-        usage()
+        input_str=" ".join(args.f)
+        print(abbrev_author_list(input_str))
+        exit(0)
+
+    for outline in process_bib(instream,perserve_author=args.keep_author):
+        print(outline,end="")
+    instream.close()
