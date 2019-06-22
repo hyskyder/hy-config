afb8daad9d4b71f9ffc2466d2f6929fabc375dc8
hy <lshhwj@gmail.com>
2019-06-22 21:11:01 +0800

    feat(script): teleself smart send_file

--- bin_scripts/scripts/teleself.sh
+++ bin_scripts/scripts/teleself.sh
@@ -4,7 +4,7 @@ HERE="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 # ABS_SCRIPT_PATH="$(dirname "$(readlink --canonicalize "${BASH_SOURCE[0]}")" )"
 [[ $1 = "-D" ]] && shift && set -x
 
-function if_is_known_command {
+function is_known_command {
     KNOWN_CMD=("msg" "fwd" "fwd_media" "send_audio" "send_document" "send_file" "send_photo" "send_text" "send_video")
     for c in "${KNOWN_CMD[@]}" ; do 
         [[ $1 == "$c" ]] && return 0
@@ -12,10 +12,14 @@ function if_is_known_command {
     return 1
 }
 
-TELE_CMD="msg"
-if_is_known_command "$1" && \
+if is_known_command "$1" ; then
     TELE_CMD=$1 && \
     shift
+elif [[ ( -z $TELE_CMD ) && ( $# -eq 1 ) && ( -r $1 ) ]] ; then
+    TELE_CMD="send_file"
+else
+    TELE_CMD="msg"
+fi
 
 [[ -f ${HERE}/teleself.config.ini ]] && SELF_ID=$(cat "${HERE}/teleself.config.ini")
 [[ -z ${SELF_ID} ]] && read -r -p "Your Telegram User id#:" SELF_ID
