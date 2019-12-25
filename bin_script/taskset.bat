609ae7fef14d25a067493046a799696f8d91f26f
hy <wheac@connect.ust.hk>
2019-12-25 18:50:16 +0800

    feat: simple core mask validation.

--- bin_script/taskset.bat
+++ bin_script/taskset.bat
@@ -7,5 +7,11 @@ REM       [/LOW | /NORMAL | /HIGH | /REALTIME | /ABOVENORMAL | /BELOWNORMAL]
 REM       [/NODE <NUMA node>] [/AFFINITY <hex affinity mask>] [/WAIT] [/B]
 REM       [command/program] [parameters]
 
+echo %1%| findstr /r "^0[xX][0123456789][0123456789]*$ ^[0123456789][0123456789]*$" > NUL & IF ERRORLEVEL 1 (
+    ECHO [Error] invalid core_mask: %1%
+    ECHO usage: %0% ^<core_mask^> ^<cmd [args]^> 
+    EXIT 1
+)
+
 START "" /I /WAIT /B /AFFINITY %*
 EXIT
