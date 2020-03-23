e3fd2abf728f4d92433466181bb49546d8c54be6
hy <lshhwj@gmail.com>
2020-03-23 16:31:35 +0800

    fix: parameter name conflict.

--- bin_script/taskset.ps1
+++ bin_script/taskset.ps1
@@ -1,31 +1,32 @@
 Param(
-    [Parameter(Mandatory=$true)]
-    [Int32] $CoreMask,
+    [Parameter(Position=0,Mandatory=$true)]
+    [Int32] $_CoreMask,
 
-    [Parameter(Mandatory=$true)]
-    [string] $ExePath,
+    [Parameter(Position=1,Mandatory=$true)]
+    [string] $_ExePath,
 
     [Parameter(Mandatory=$false,ValueFromRemainingArguments)]
-    [array] $ArgumentList
+    [array] $_ArgumentList
 )
 
 $thisProcess = [System.Diagnostics.Process]::GetCurrentProcess();
-$thisProcess.ProcessorAffinity = $CoreMask;
+$thisProcess.ProcessorAffinity = $_CoreMask;
+$GotAffinity=Select-Object -inputObject $thisProcess ProcessorAffinity
+Write-Verbose ">> Set ProcessorAffinity to $_CoreMask, Got ${GotAffinity}"
 
-
-if(Get-Command $ExePath -Type Cmdlet -WarningAction:SilentlyContinue -errorAction SilentlyContinue){
-    Write-Warning "[WARN] $ExePath is a cmdlet."
+if(Get-Command $_ExePath -Type Cmdlet -WarningAction:SilentlyContinue -errorAction SilentlyContinue){
+    Write-Warning "[WARN] $_ExePath is a cmdlet."
     $args=""
-    if($ArgumentList){
-        $args=[string]::Join(" ",$ArgumentList)
+    if($_ArgumentList){
+        $args=[string]::Join(" ",$_ArgumentList)
     }
-    $cmd+="$ExePath "+$args
+    $cmd+="$_ExePath "+$args
     Invoke-Expression $cmd
-} Elseif ( $ArgumentList ){
-    Write-Verbose ">> $ExePath $ArgumentList"
-    Start-Process -NoNewWindow -Wait $ExePath -ArgumentList $ArgumentList
+} Elseif ( $_ArgumentList ){
+    Write-Verbose ">> $_ExePath $_ArgumentList"
+    Start-Process -NoNewWindow -Wait $_ExePath -ArgumentList $_ArgumentList
 } Else {
-    Write-Verbose ">> $ExePath"
-    Start-Process -NoNewWindow -Wait $ExePath
+    Write-Verbose ">> $_ExePath"
+    Start-Process -NoNewWindow -Wait $_ExePath
 }
 
